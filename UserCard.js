b794d12efe492913911578ec7e3d28ebda2c98b6
biagavirete <biagavirete@gmail.com>
2021-01-16 14:02:52 -0300

    feat: add navbar info

--- UserCard.js
+++ UserCard.js
@@ -1,12 +1,13 @@
 class UserCard {
   constructor() {
-    this.dados_usuario = document.querySelector(".profile-header")
+    this.user_info = document.querySelector(".profile-header")
+    this.user_status = document.querySelector("#profile-status")
   }
 
   showUser(data) {
-    let card = ''
+    let profile_card = ''
 
-    card = `
+    profile_card = `
       <h3 class="profile-fullname"><a>${data[0].nome}<a></h3>
             <h2 class="profile-element"><a>@${data[0].usuario}</a></h2>
             <a class="profile-element profile-website" hrerf=""><i
@@ -17,6 +18,38 @@ class UserCard {
             <button class="btn btn-search-bar tweet-to-btn">Tweet to ${data[0].nome}</button>
       `;
 
-    this.dados_usuario.innerHTML = card
+    let navbar_card = ''
+
+    navbar_card = `
+    <ul>
+    <li class="profile-stats-item-active">
+      <a>
+        <span class="profile-stats-item profile-stats-item-label">Tweets</span>
+        <span class="profile-stats-item profile-stats-item-number">${data[0].quantidade_tweets}</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span class="profile-stats-item profile-stats-item-label">Following</span>
+        <span class="profile-stats-item profile-stats-item-number">${data[0].seguindo}</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span class="profile-stats-item profile-stats-item-label">Followers</span>
+        <span class="profile-stats-item profile-stats-item-number">${data[0].seguidores}</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span class="profile-stats-item profile-stats-item-label">Likes</span>
+        <span class="profile-stats-item profile-stats-item-number">${data[0].likes}</span>
+      </a>
+    </li>
+  </ul>
+    `
+
+    this.user_info.innerHTML = profile_card
+    this.user_status.innerHTML = navbar_card
   }
 }
